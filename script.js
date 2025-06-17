59ce3d06c999c0b6df9210b9967c20ecc0b71706
Ozioma Osuagwu <ifeanyiozioma2019@gmail.com>
2025-06-17 14:43:24 -0700

    new changes

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const removeButton = document.createElement('button');
         removeButton.textContent = 'Remove';
-        removeButton.className = 'remove-btn';
+        removeButton.classList.add = 'remove-btn';
 
 
         removeButton.onclick = function () {
